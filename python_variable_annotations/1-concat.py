ce0916665edb6b377ec3346b5f6b8248fe4d75ce
heydarov93 <heyderov.yashar@mail.ru>
2024-04-29 12:40:32 +0400

    Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Module that contains type-annotated function to
+concatenate strings and to return string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    return str1 + str2
