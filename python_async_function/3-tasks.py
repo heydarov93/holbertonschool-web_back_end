63fdc4bf5fa47ea6f774e6f5949a4cb74bf1ceef
heydarov93 <heyderov.yashar@mail.ru>
2024-04-30 18:03:26 +0400

    Function that creates and returns asyncio task

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module containes regular function
+that returns asyncio.Task
+"""
+import asyncio
+from typing import Awaitable
+
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+def task_wait_random(max_delay: int) -> Awaitable:
+    """
+    Function that takes an integer max_delay
+    and returns a asyncio.Task
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
