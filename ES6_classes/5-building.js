c401fe87af9a7b3c90837f85deab0196f7c64647
heydarov93 <heyderov.yashar@mail.ru>
2024-05-14 17:24:37 +0400

    Imitation of abstract class

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,25 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    this.evacuationWarningMessage();
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+
+    this._sqft = sqft;
+  }
+
+  evacuationWarningMessage() {
+    if (this.constructor !== Building) {
+      const msg = 'Class extending Building must override evacuationWarningMessage';
+      throw new Error(msg);
+    }
+  }
+}
