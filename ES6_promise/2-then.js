bd72799cacac2487bdaef3b7d2391c14cd0d7bb2
heydarov93 <heyderov.yashar@mail.ru>
2024-05-20 15:37:48 +0400

    return statement added after promise

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,19 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(
-    () => ({
-      status: 200,
-      body: 'Success',
-    }),
-    () => new Error(),
-  ).finally(() => {
-    console.log('Got a response from the API');
-  });
-  // promise.then(() => ({
-  //   status: 200,
-  //   body: 'Success',
-  // }))
-  //   .catch(() => new Error())
-  //   .finally(() => {
-  //     console.log('Got a response from the API');
-  //   });
+  return promise.then(() => ({
+    status: 200,
+    body: 'Success',
+  }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
