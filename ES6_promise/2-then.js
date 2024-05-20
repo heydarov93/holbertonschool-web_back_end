d3eb720a278f7303a1812601af62d68b2891d176
heydarov93 <heyderov.yashar@mail.ru>
2024-05-20 15:10:45 +0400

    implementation changed

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,19 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
-    status: 200,
-    body: 'Success',
-  }))
-    .catch(() => new Error())
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+  promise.then(
+    () => ({
+      status: 200,
+      body: 'Success',
+    }),
+    () => new Error(),
+  );
+
+  console.log('Got a response from the API');
+  // promise.then(() => ({
+  //   status: 200,
+  //   body: 'Success',
+  // }))
+  //   .catch(() => new Error())
+  //   .finally(() => {
+  //     console.log('Got a response from the API');
+  //   });
 }
