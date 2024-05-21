3267ba94fa968f76d5d19b09e0bbc289a2f004f7
heydarov93 <heyderov.yashar@mail.ru>
2024-05-21 16:20:35 +0400

    reason property inside second object replaced by value property

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,5 +4,12 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
-  return Promise.allSettled(promises).then((result) => result);
+  return Promise
+    .allSettled(promises).then((result) => (result.map((obj) => {
+      if (Object.hasOwn(obj, 'reason')) {
+        return { status: obj.status, value: String(obj.reason) };
+      }
+      return obj;
+    })
+    ));
 }
